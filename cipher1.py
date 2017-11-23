ab15e1d2c98a98e0c8dadcafef83252f53b3468b
Ubuntu <ubuntu@ip-172-31-24-13.us-west-2.compute.internal>
2017-11-23 17:27:12 +0000

    Test Decipher script

---
+++ cipher1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is doc String
+
+"""
+
+print("This is test")
+arg = input("ENter a string : ")
+arg.lower()
+l = len(arg) -1
+sarg = []
+def decipher(arg):
+    for let in arg:
+        if let == 'z'
+            neword.append(char(97 + 2)
+        else:
+            neword.append(chr(ord(let)+2)
+    print(neword)
+decipher(arg)
+
+
+# To be continued
+
